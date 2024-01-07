6a4acddc54771201b4cd36b3e4d5814753bbfab2
01723710909 <137289906+01723710909@users.noreply.github.com>
2024-01-07 07:59:08 +0600

    2nd

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto text-center margin-auto '>
+            <ul className="steps steps-vertical">
+                <li className="step step-primary">Register</li>
+                <li className="step step-primary">Choose plan</li>
+                <li className="step">Purchase</li>
+                <li className="step">Receive Product</li>
+            </ul>
         </div>
     );
 };
